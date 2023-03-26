dabd49966d051fff93d9ec7a3b9f7114e84a1a9b
xpzhang <xp17126@163.com>
2023-03-26 22:09:51 +0800

    23_3_26

---
+++ study/HzOJ/86.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xp.Zhang
+ * @Date: 2023-03-26 22:03:32
+ * @LastEditors: xp.Zhang
+ * @LastEditTime: 2023-03-26 22:04:49
+ */
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a * b * c << endl;
+}
---
+++ study/HzOJ/87.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xp.Zhang
+ * @Date: 2023-03-26 22:05:44
+ * @LastEditors: xp.Zhang
+ * @LastEditTime: 2023-03-26 22:07:44
+ */
+//setprecision：功能是控制输出流显示浮点数的有效数字个数，注意是有效数字。如果和fixed合用的话，可以控制小数点后面有几位。
